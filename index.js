120e37ace0692189936c8db8b6c94486af7b5cbc
pavelsmirnov77 <64857615+pavelsmirnov77@users.noreply.github.com>
2021-03-23 01:29:52 +0300

    fix for Heroku

--- index.js
+++ index.js
@@ -55,7 +55,6 @@ const Zdrav = mongoose.model('zdrav')
 //Heroku
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
 
 //For avoidong Heroku $PORT error
 app.get('/', function (request, response) {
